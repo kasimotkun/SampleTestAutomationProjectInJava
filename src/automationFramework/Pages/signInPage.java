3720be23a649a6e9273ed2d12209cce19350cc37
kasimotkun <kasimotkun@gmail.com>
2020-07-24 19:52:19 +0300

    Update signInPage.java

--- src/automationFramework/Pages/signInPage.java
+++ src/automationFramework/Pages/signInPage.java
@@ -5,6 +5,8 @@ import java.util.Set;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -14,7 +16,20 @@ public class signInPage extends pageBase {
 	
 	private WebDriver driver;
 	private WebDriverWait wait;
+	
+	@FindBy(how = How.ID, using = "passwd")
+	private WebElement passwd;	
+	
+	@FindBy(how = How.ID, using = "SubmitLogin")
+	private WebElement SubmitLogin;	
 
+	@FindBy(how = How.ID, using = "SubmitCreate")
+	private WebElement SubmitCreate;
+	
+	private By email = By.id("email");
+	
+	private By email_create = By.id("email_create");
+	
 	public signInPage(WebDriver driver, WebDriverWait wait) {
 		
 		this.driver=driver;
@@ -24,17 +39,17 @@ public class signInPage extends pageBase {
 	
 	public myAccountPage signIn(String username, String pwd)
 	{
-		 wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email"))).sendKeys(username);
-		 driver.findElement(By.id("passwd")).sendKeys(pwd);
-		 driver.findElement(By.id("SubmitLogin")).click();
+		 wait.until(ExpectedConditions.visibilityOfElementLocated(email)).sendKeys(username);
+		 passwd.sendKeys(pwd);
+		 SubmitLogin.click();
 		 
 		 return new myAccountPage(driver,wait);
 	}
 	
 	public createNewUserPage gotoCreateNewUser(String email)
 	{
-		 wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email_create"))).sendKeys(email);
-		 driver.findElement(By.id("SubmitCreate")).click();
+		 wait.until(ExpectedConditions.visibilityOfElementLocated(email_create)).sendKeys(email);
+		 SubmitCreate.click();
 		 
 		 return new createNewUserPage(this.driver,this.wait);
 	}
