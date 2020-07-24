da1a7a1e8314ee4015de1027c81b4e9ca13b1f4a
kasimotkun <kasimotkun@gmail.com>
2020-07-24 19:52:44 +0300

    Update myAccountPage.java

--- src/automationFramework/Pages/myAccountPage.java
+++ src/automationFramework/Pages/myAccountPage.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -15,6 +18,20 @@ public class myAccountPage extends pageBase {
   private WebDriver driver;
   private WebDriverWait wait;
   
+  @FindBy(how = How.XPATH, using = "//a[@title='\"+hashMapValue.get(\"productName\")+\"']")
+  private WebElement productName;
+  
+  @FindBy(how = How.NAME, using = "processCarrier")
+  private WebElement processCarrier;
+  
+  private By Submit = By.name("Submit");
+  private By Proceedtocheckout1 =  By.xpath("//*[@id='layer_cart']//a[@class and @title='Proceed to checkout']");
+  private By Proceedtocheckout2 = By.xpath("//*[contains(@class,'cart_navigation')]/a[@title='Proceed to checkout']");
+  private By processAddress = By.name("processAddress");
+  private By uniformcgv = By.id("uniform-cgv"); 
+  private By bankwire =By.className("bankwire");
+  private By button = By.xpath("//*[@id='cart_navigation']/button");
+  
    public myAccountPage(WebDriver driver, WebDriverWait wait) {
 	   
 	   this.driver=driver;
@@ -23,27 +40,27 @@ public class myAccountPage extends pageBase {
 
    public void checout( HashMap<String,String> hashMapValue)
    {
-	   
+	   	 PageFactory.initElements(driver, this);
+	   	 
 		 wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText(hashMapValue.get("category")))).click();
-		 driver.findElement(By.xpath("//a[@title='"+hashMapValue.get("productName")+"']")).click();
-		 wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("Submit"))).click();
-		 wait.until(ExpectedConditions.visibilityOfElementLocated(
-				 By.xpath("//*[@id='layer_cart']//a[@class and @title='Proceed to checkout']"))).click();
+		 productName.click();
+		 
+		 wait.until(ExpectedConditions.visibilityOfElementLocated(Submit)).click();
+		 wait.until(ExpectedConditions.visibilityOfElementLocated(Proceedtocheckout1)).click();
 
-		 wait.until(ExpectedConditions.visibilityOfElementLocated(
-				 By.xpath("//*[contains(@class,'cart_navigation')]/a[@title='Proceed to checkout']"))).click();
+		 wait.until(ExpectedConditions.visibilityOfElementLocated(Proceedtocheckout2)).click();
 
 		
 
-		 wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("processAddress"))).click();
-		 wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("uniform-cgv"))).click();
+		 wait.until(ExpectedConditions.visibilityOfElementLocated(processAddress)).click();
+		 wait.until(ExpectedConditions.visibilityOfElementLocated(uniformcgv)).click();
 
 
-		 driver.findElement(By.name("processCarrier")).click();
+		 processCarrier.click();
 
 
-		 wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("bankwire"))).click();
-		 wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='cart_navigation']/button")))
+		 wait.until(ExpectedConditions.visibilityOfElementLocated(bankwire)).click();
+		 wait.until(ExpectedConditions.visibilityOfElementLocated(button))
 				 .click();
    }
 		
